95453172c6b1239bf24e04b39552b56f38707c4b
AnaJuvencio <ana.juvencio01@gmail.com>
2024-04-26 21:01:15 -0300

    melhorando o codigo para respeitar as triggers

--- base-de-dados.py
+++ base-de-dados.py
@@ -4,11 +4,12 @@ from random import randint
 from datetime import datetime
 
 #OBS: 
-# 1º Precisa verificar se está inserindo as data corretamente, na tabela Horário coloquei um exemplo de como ficaria, mas precisa ver se nas outras tabela tbm está certo;
-# 2º O try Exception foi inserido por conta da trigger em compra
+# 1º Precisa verificar se está inserindo as datas corretamente, na tabela Horário coloquei um exemplo de como ficaria, coloquei oq é pra ser a estrutura correta
+# 2º Verificar se está respeitando as duas triggers 
 # 3º Verificar se está violando a restrição de integridade referencial
-  
-# Conectando ao banco de dados
+# 4º Criar mecanismo que tente gerar os dados novamente em caso de erro de inserção (por segurança)
+
+# Conexão com o banco de dados
 conn = psycopg2.connect(
     dbname="empresa_de_transportes",
     user="seu_usuario",
@@ -17,87 +18,80 @@ conn = psycopg2.connect(
     port="5432"
 )
 
-# Criando um cursor
+# Cursor
 cur = conn.cursor()
 
-# Instanciando o Faker
+# Faker para gerar dados falsos
 fake = Faker()
 
-# Inserindo dados na tabela Cliente
+# Função para inserir dados 
+def inserir_dados(tabela, dados):
+    try:
+        cur.execute(f"INSERT INTO {tabela} VALUES ({', '.join(['%s'] * len(dados))})", dados)
+        conn.commit()
+    except psycopg2.Error as e:
+        print(f"Erro ao inserir dados na tabela {tabela}: {e}")
+        conn.rollback()
+
+# Inserção de dados na tabela Cliente
 for _ in range(10):
     cpf = fake.unique.random_number(digits=11)
     nome = fake.name()
     endereco = fake.address()
     telefone = fake.phone_number()
     email = fake.email()
-    
-    cur.execute("INSERT INTO Cliente (CPF, Nome, Endereco, Telefone, Email) VALUES (%s, %s, %s, %s, %s)",
-                (cpf, nome, endereco, telefone, email))
+    inserir_dados("Cliente", (cpf, nome, endereco, telefone, email))
 
-# Inserindo dados na tabela Cidade
+# Inserção de dados na tabela Cidade
 for _ in range(10):
-    Nome_Cidade = fake.city()
-    cur.execute("INSERT INTO Cidade (Nome_Cidade) VALUES (%s)", (Nome_Cidade))
+    nome_cidade = fake.city()
+    inserir_dados("Cidade", (nome_cidade,))
 
-# Inserindo dados na tabela Horario
+# Inserção de dados na tabela Horario
 for _ in range(10):
-    data_hora_chegada = fake.future_datetime(end_date='+30d').strftime('%Y-%m-%d %H:%M:%S') #"YYYY-MM-DD HH:MM:SS"
+    data_hora_chegada = fake.future_datetime(end_date='+30d').strftime('%Y-%m-%d %H:%M:%S')
     data_hora_partida = fake.date_time_between(start_date=data_hora_chegada, end_date=data_hora_chegada).strftime('%Y-%m-%d %H:%M:%S')
-    
-    cur.execute("INSERT INTO Horario (Data_Hora_Chegada, Data_Hora_Partida) VALUES (%s, %s)",
-                (data_hora_chegada, data_hora_partida))
+    inserir_dados("Horario", (data_hora_chegada, data_hora_partida))
 
-# Inserindo dados na tabela Rotas
+# Inserção de dados na tabela Rotas
 for _ in range(10):
-    Nome_Cidade_Origem, Nome_Cidade_Destino = fake.random.choice([row for row in cur.execute("SELECT Nome_Cidade_Origem, Nome_Cidade_Destino FROM Cidade")])
+    nome_cidade_origem = fake.random.choice([row[0] for row in cur.execute("SELECT Nome_Cidade FROM Cidade")])
+    nome_cidade_destino = fake.random.choice([row[0] for row in cur.execute("SELECT Nome_Cidade FROM Cidade")])
+    while nome_cidade_origem == nome_cidade_destino:  # Garante que as cidades de origem e destino sejam diferentes
+        nome_cidade_destino = fake.random.choice([row[0] for row in cur.execute("SELECT Nome_Cidade FROM Cidade")])
     distancia = randint(100, 1000)
     preco = randint(50, 200)
-    
-    cur.execute("INSERT INTO Rotas (Origem, Destino, Distancia, Preco) VALUES (%s, %s, %s, %s)",
-                (Nome_Cidade_Origem, Nome_Cidade_Destino, distancia, preco))
+    inserir_dados("Rotas", (nome_cidade_origem, nome_cidade_destino, distancia, preco))
 
-# Inserindo dados na tabela Veiculos
+# Inserção de dados na tabela Veiculos
 for _ in range(5):
     placa = fake.unique.license_plate()
     modelo = fake.car_model()  
     ano = fake.random.randint(2010, 2023)
     status_vei = fake.random.choice(["disponivel", "em manutencao"])
     capacidade = randint(1, 60)
-    
-    cur.execute("INSERT INTO Veiculos (Placa, Modelo, Ano, Status_vei, Capacidade) VALUES (%s, %s, %s, %s, %s)",
-                (placa, modelo, ano, status_vei, capacidade))
+    inserir_dados("Veiculos", (placa, modelo, ano, status_vei, capacidade))
 
-# Inserindo dados na tabela Compra
+# Inserção de dados na tabela Compra
 for _ in range(10):
     cpf = fake.unique.random_number(digits=11)
     data_hora_chegada = fake.future_datetime(end_date='+30d').strftime('%Y-%m-%d %H:%M:%S')
     data_hora_partida = fake.date_time_between(start_date=data_hora_chegada, end_date=data_hora_chegada).strftime('%Y-%m-%d %H:%M:%S')
     assento = randint(1, 59)
     status_ag = fake.random.choice(["confirmado", "cancelado", "realizado"])
-    
-    try: #respeita a trigger verificar_capacidade_antes_insercao
-        cur.execute("INSERT INTO Compra (CPF, Data_Hora_Chegada, Data_Hora_Partida, Assento, Status_ag) VALUES (%s, %s, %s, %s, %s)",
-                    (cpf, data_hora_chegada, data_hora_partida, assento, status_ag))
-        conn.commit()  # Commit se não houver exceção
-    except psycopg2.Error as e:
-        print("Erro ao inserir compra:", e)
-        conn.rollback()  # Desfaz qualquer operação pendente
-        
-# Inserindo dados na tabela Possui
-for _ in range(10):
-    data_hora_chegada, data_hora_partida, Nome_Cidade_Origem, Nome_Cidade_Destino = fake.random.choice([row for row in cur.execute("SELECT Data_Hora_Chegada, Data_Hora_Partida, Nome_Cidade_Origem, Nome_Cidade_Destino FROM Horario, Rotas")])
-    
-    cur.execute("INSERT INTO Possui (Data_Hora_Chegada, Data_Hora_Partida, Nome_Cidade_Origem, Nome_Cidade_Destino) VALUES (%s, %s, %s, %s)",
-                (data_hora_chegada, data_hora_partida, Nome_Cidade_Origem, Nome_Cidade_Destino))
+    inserir_dados("Compra", (cpf, data_hora_chegada, data_hora_partida, assento, status_ag))
 
-# Inserindo dados na tabela Realizado_por
+# Inserção de dados na tabela Possui
+for _ in range(10):
+    data_hora_chegada, data_hora_partida, nome_cidade_origem, nome_cidade_destino = fake.random.choice([row for row in cur.execute("SELECT Data_Hora_Chegada, Data_Hora_Partida, Nome_Cidade_Origem, Nome_Cidade_Destino FROM Horario, Rotas")])
+    inserir_dados("Possui", (data_hora_chegada, data_hora_partida, nome_cidade_origem, nome_cidade_destino))
+
+# Inserção de dados na tabela Realizado_por
 for _ in range(10):
     placa = fake.random.choice([row[0] for row in cur.execute("SELECT Placa FROM Veiculos")])
-    Nome_Cidade_Origem, Nome_Cidade_Destino = fake.random.choice([row for row in cur.execute("SELECT Nome_Cidade_Origem, Nome_Cidade_Destino FROM Rotas")])
-    
-    cur.execute("INSERT INTO Realizado_por (Placa, Origem, Destino) VALUES (%s, %s, %s)",
-                (placa, Nome_Cidade_Origem, Nome_Cidade_Destino))
+    nome_cidade_origem, nome_cidade_destino = fake.random.choice([row for row in cur.execute("SELECT Nome_Cidade_Origem, Nome_Cidade_Destino FROM Rotas")])
+    inserir_dados("Realizado_por", (placa, nome_cidade_origem, nome_cidade_destino))
 
-conn.commit() #literalmente insere no bd
-cur.close() #fecha o cursor que se comunica com o bd
-conn.close() # economizar recursos
+# Fechar conexão e cursor
+cur.close()
+conn.close()
